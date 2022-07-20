37a9577cd751082f68bf98ffbb678a441ad1e60e
Fonseca <matheusfonsecade@hotmail.com>
2022-07-20 03:29:09 -0300

    requisito 3 completo + lint

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,5 +4,5 @@ export const LOGIN = 'LOGIN';
 export const actLog = (email) => ({ type: LOGIN, email });
 
 export const currency = (currencie) => ({
-    type: CURRENCY, currencie
+  type: CURRENCY, currencie,
 });
