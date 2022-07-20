f9d3a0f5fc6abda576f1ea087bd686577ad27b4e
Fonseca <matheusfonsecade@hotmail.com>
2022-07-20 06:29:34 -0300

    correção

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const ADD_EXPENSE = 'ADD_EXPENSE';
 export const DELETE_EXPENSE = 'DELETE_EXPENSE';
 export const EDIT_EXPENSE = 'EDIT_EXPENSE';
 
-export const userLoginAction = (email) => (
+export const actLog = (email) => (
   {
     type: LOGIN,
     payload: email,
