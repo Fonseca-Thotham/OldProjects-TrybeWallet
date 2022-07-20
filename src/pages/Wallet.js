94aa66467d27e1479fcb6867b062d45facf1ff8e
Fonseca <matheusfonsecade@hotmail.com>
2022-07-20 04:08:43 -0300

    correção req 1 e conclusão req 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -13,17 +13,72 @@ class Wallet extends React.Component {
     const { email, currency } = this.props;
     return (
       <div>
-        <p data-testid="email-field">
-          Email:
+        <header>
+          <p data-testid="email-field">
+            Email:
+            {' '}
+            {email}
+          </p>
+          <p data-testid="total-field">despesa: 0</p>
+          <p data-testid="header-currency-field">
+            moeda:
+            {' '}
+            BRL
+          </p>
+        </header>
+        <label htmlFor="despesas">
+          Valor:
           {' '}
-          {email}
-        </p>
-        <p data-testid="total-field">despesa: 0</p>
-        <p data-testid="header-currency-field">
-          moeda:
-          { 'BRL' }
-          { currency }
-        </p>
+          <input
+            data-testid="value-input"
+            type="number"
+            id="despesa"
+          />
+        </label>
+        <label htmlFor="despesaid">
+          Descrição:
+          {' '}
+          <input
+            data-testid="description-input"
+            type="text"
+            id="despesaid"
+          />
+        </label>
+        <label htmlFor="currencyid">
+          Moeda:
+          {' '}
+          <select id="currencyid">
+            {currency.map((currencies, index) => (
+              <option value={ currencies } key={ index }>{currencies}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="currencyid">
+          Método de pagamentos:
+          {' '}
+          <select data-testid="method-input" id="currencyid">
+            <option>
+              Dinheiro
+            </option>
+            <option>
+              Cartão de crédito
+            </option>
+            <option>
+              Cartão de débito
+            </option>
+          </select>
+        </label>
+        <label data-testid="tag-input" htmlFor="currencyid">
+          Categoria:
+          {' '}
+          <select id="currencyid">
+            <option>Alimentação</option>
+            <option>Lazer</option>
+            <option>Trabalho</option>
+            <option>Transporte</option>
+            <option>Saúde</option>
+          </select>
+        </label>
       </div>
     );
   }
